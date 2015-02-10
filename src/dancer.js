03d4903cf8111e041fa8ffa5bdee0e29fc8668b0
KalevRK <KalevRK@gmail.com>
2015-02-09 16:18:12 -0800

    Add top, left parameters to class constructors.

--- src/dancer.js
+++ src/dancer.js
@@ -33,11 +33,14 @@
 };*/
 
 
-var Dancer = function(timeBetweenSteps) {
+var Dancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this._timeBetweenSteps = timeBetweenSteps;
+  this._top = top;
+  this._left = left;
 
   this.step(); // initiate loop of step calls
+  this.setPosition(this._top, this._left); // initialize position
 };
 
 Dancer.prototype.step = function() {
