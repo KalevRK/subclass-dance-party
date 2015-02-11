3d8fc8322aa017963418f1a1faaa1f2fc74aacc9
KalevRK <KalevRK@gmail.com>
2015-02-10 17:17:44 -0800

    Implement surround behavior for GrumpyCatDancer.

--- src/grumpyCatDancer.js
+++ src/grumpyCatDancer.js
@@ -3,6 +3,7 @@ var GrumpyCatDancer = function(top, left) {
   this.$node.addClass('grumpyCat');
   this._size = 1;
   this._growing = true;
+  this._surrounding = false;
   this._id = GrumpyCatDancer.prototype.grumpyCount++;
 };
 
@@ -12,6 +13,11 @@ GrumpyCatDancer.prototype.constructor = GrumpyCatDancer;
 GrumpyCatDancer.prototype.step = function() {
   CatDancer.prototype.step.call(this);
   this.$node.css(this.resize());
+
+  if (!this._surrounding && GrumpyCatDancer.prototype.grumpyCount > 5) {
+    this.surround();
+    this._surrounding = true;
+  }
 };
 
 GrumpyCatDancer.prototype.resize = function() {
@@ -34,4 +40,11 @@ GrumpyCatDancer.prototype.lineUp = function() {
   this._top = 200 + this._id * 100;
   this._left = 800;
   this.setPosition(this._top, this._left);
+};
+
+GrumpyCatDancer.prototype.surround = function() {
+  this._top = 500;
+  this._left = 500;
+  this._theta = (2 * Math.PI / GrumpyCatDancer.prototype.grumpyCount) * this._id;
+  this._radius = 10;
 };
